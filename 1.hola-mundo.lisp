8310a7f24268500dae7d61b25f75f9ada96492c0
Moi Rouhs <carsrouhs@gmail.com>
2017-11-08 16:46:10 +0000

    Imprimir hola mundo en consola

---
+++ 1.hola-mundo.lisp
@@ -0,0 +1,4 @@
+(format t "Hola mundo desde format") ; Permite imprimir variables sin tener que concatenarlas
+(print "Hola mundo desde print") ; Imprime en consola sin salto de linea
+(write "Hola mundo desde write") ; Imprimer cadenas de texto entre comillas dobles en consola sin salto de linea
+(write-line "Hola mundo") ; Imprime cadenas de texto en consola con salto de linea.
